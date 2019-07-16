2b297ecb0102114eac92a43bcc22c50bb5673917
skings <samanf74@gmail.com>
2019-07-16 14:59:10 +0430

    adding to cache is now supported

--- src/server/main.go
+++ src/server/main.go
@@ -2,8 +2,18 @@ package main
 
 import (
 	"../shortener"
+	"fmt"
+	gocache "github.com/patrickmn/go-cache"
 )
 
 func main() {
-	shortener.Add("hhhh")
+	cache := gocache.New(gocache.NoExpiration, gocache.NoExpiration)
+	for i := 0; i < 50; i++ {
+		a, ok := shortener.Add("hhhh", cache)
+		if !ok {
+			fmt.Println("FUCK")
+		}
+		fmt.Println(a)
+	}
+
 }
--- src/shortener/shortener.go
+++ src/shortener/shortener.go
@@ -3,13 +3,18 @@ package shortener
 import (
 	"crypto/sha1"
 	"encoding/base64"
-	"fmt"
+	gocache "github.com/patrickmn/go-cache"
 	"net/url"
 	"strings"
 	"time"
 )
 
-func Add(longUrl string) (string, bool) {
+type address struct {
+	url    string
+	clicks int64
+}
+
+func Add(longUrl string, cache *gocache.Cache) (string, bool) {
 	if longUrl == "" {
 		return "", false
 	}
@@ -19,7 +24,29 @@ func Add(longUrl string) (string, bool) {
 	sha = strings.ToUpper(sha)
 	key := url.PathEscape(sha)
 
-	fmt.Println(key)
+	index := 5
+	for {
+		if !(isExist(key[:index], cache)) {
+			break
+		}
+		index++
+		if index == len(key) {
+			hasher.Write([]byte(longUrl + time.Now().String()))
+			sha = base64.URLEncoding.EncodeToString(hasher.Sum(nil))
+			sha = strings.ToUpper(sha)
+			key += url.PathEscape(sha)
+		}
+	}
 
-	return "", true
+	err := cache.Add(key[:index], address{url: longUrl, clicks: 0}, gocache.NoExpiration)
+	if err != nil {
+		return "", false
+	}
+
+	return key[:index], true
+}
+
+func isExist(key string, c *gocache.Cache) bool {
+	_, isExist := c.Get(key)
+	return isExist
 }
