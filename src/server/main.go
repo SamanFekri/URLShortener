39bc47c21ae9ce9f5c385be447cfba425ee62578
skings <samanf74@gmail.com>
2019-07-16 14:12:36 +0430

    now we create the key suitable for the URL

--- src/server/main.go
+++ src/server/main.go
@@ -1,4 +1,9 @@
 package main
 
+import (
+	"../shortener"
+)
+
 func main() {
+	shortener.Add("hhhh")
 }
--- src/shortener/shortener.go
+++ src/shortener/shortener.go
@@ -1,7 +1,25 @@
 package shortener
 
-import "fmt"
+import (
+	"crypto/sha1"
+	"encoding/base64"
+	"fmt"
+	"net/url"
+	"strings"
+	"time"
+)
 
-func Print(s string) {
-	fmt.Println(s)
+func Add(longUrl string) (string, bool) {
+	if longUrl == "" {
+		return "", false
+	}
+	hasher := sha1.New()
+	hasher.Write([]byte(longUrl + time.Now().String()))
+	sha := base64.URLEncoding.EncodeToString(hasher.Sum(nil))
+	sha = strings.ToUpper(sha)
+	key := url.PathEscape(sha)
+
+	fmt.Println(key)
+
+	return "", true
 }
